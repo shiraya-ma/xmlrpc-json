45b7f0fd7f40092365e6c9734dcb67b97acc9c19
shiraya-ma <mai-shirayama@outlook.jp>
2025-08-15 04:06:46 +0900

    :fire: remove props

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,8 +19,6 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-    },
     rules: {
       'no-empty-pattern': 'off',
       '@typescript-eslint/no-namespace': 'off',
