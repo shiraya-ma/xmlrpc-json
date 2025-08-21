96fef5285fbc7e7cf1614ca8993aebcdcd0c40ed
shiraya-ma <mai-shirayama@outlook.jp>
2025-08-21 12:35:20 +0900

    :sparkles: defined Children types

--- src/dom/_children.ts
+++ src/dom/_children.ts
@@ -1,3 +1,20 @@
 'use strict';
+import type { Node, NodeConstructorOptions } from "./node";
 
-export type WithChildren<T = {}> = T & {};
+export type WithChildren<T = {}> = T & {
+  children: Node[];
+
+  appendChild: AppendChildFunction;
+  removeChild: RemoveChildFunction;
+  replaceChild: ReplaceChildFunction;
+  insertBefore: InsertBeforeFunction;
+};
+
+export type AppendChildFunction = (node: Node) => WithChildren<Node>;
+export type RemoveChildFunction = (node: Node) => WithChildren<Node>;
+export type ReplaceChildFunction = (newNode: Node, oldNode: Node) => WithChildren<Node>;
+export type InsertBeforeFunction = (newNode: Node, refNode: Node) => WithChildren<Node>;
+
+export type WithChildrenNodeConstructorOptions = NodeConstructorOptions & Partial<{
+  children: Node[];
+}>;
